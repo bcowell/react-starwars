e2b61b13405cec962b8a12b31a814ed49a8faec3
Brayden Cowell <bcowell@hotmail.ca>
2019-05-29 11:26:59 -0400

    Added routing!

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import ShipContainer from './ShipContainer';
+
+const Routes = () => 
+    <Switch>
+        <Route path='/' component={ShipContainer} exact={true} />
+        <Route path='/ships/:id' component={ShipContainer} />
+    </Switch>
 
 const App = () => 
     <>
-        <ShipContainer />
+        <Routes />
     </>
 
 export default App;
--- src/containers/index.js
+++ src/containers/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from '../store';
+import { BrowserRouter } from 'react-router-dom';
 
 const Root = () =>
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>
 
 export default Root;
