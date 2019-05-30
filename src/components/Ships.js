d4240033d1f5f827ddb0b72bc10229dbe5734f5f
Brayden Cowell <bcowell@hotmail.ca>
2019-05-30 00:14:58 -0400

    Sweet new loading gif

--- src/components/Ships.js
+++ src/components/Ships.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Table } from 'reactstrap';
+import loadingGif from '../assets/loading.gif';
 import Ship from './Ship';
 
-
 const Ships = props =>
-
-    props.isFetching ? 'Loading...' :
-        props.ships ?
-            <Table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Model</th>
-                        <th>Manufacturer</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.ships.map((ship, i) => (
-                        <Ship ship={ship} i={i} key={i} />
-                    ))}
-
-                </tbody>
-
-            </Table>
+    props.isFetching
+        ? <img src={loadingGif} alt="loading" />
+        : props.ships
+            ? <Ship ships={props.ships} />
             : 'No ships...'
-
 
 Ships.propTypes = {
     ships: PropTypes.array
