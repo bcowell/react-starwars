5ffe5116eea65a6df284e316c8edd038ab868ea9
Brayden Cowell <bcowell@hotmail.ca>
2019-05-31 10:50:17 -0400

    Switched to MemoryRouter for netlify live demo

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from '../store';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 const Root = () =>
     <Provider store={store}>
-        <BrowserRouter>
+        <MemoryRouter>
             <App />
-        </BrowserRouter>
+        </MemoryRouter>
     </Provider>
 
 export default Root;
